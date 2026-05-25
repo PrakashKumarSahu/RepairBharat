64ebea11de3e23cf6e43f588fc3464cdc4d9c7be
Prakash Kumar <jaysahu0201@gmail.com>
2026-05-25 11:50:56 +0000

    Database migrated from sqlite to psql

--- backend/RepairBharat/settings.py
+++ backend/RepairBharat/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'RepairBharat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'repairbharatdb',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
